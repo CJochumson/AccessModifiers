97e650076903b539b0252ee32f2ab64a70fa583c
Chris Jochumson <chris.jochumson@gmail.com>
2014-05-19 10:25:17 -0700

    After adding initial code

--- AccessModifiers/AccessModifiers/Program.cs
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AccModLib;
 
 namespace AccessModifiers
 {
@@ -10,6 +11,28 @@ namespace AccessModifiers
     {
         static void Main(string[] args)
         {
+            Shape shape = new Shape();
+            shape.indexPublic = 0;
+            //shape.indexProtected = 0;
+            //shape.indexInternal = 0;
+            //shape.indexProtectedInternal = 0;
+            Shape rect = new Rect();
+            rect.indexPublic = 0;
+            //rect.indexProtected = 0;
+            //rect.indexInternal = 0;
+            //rect.indexProtectedInternal = 0;
         }
+
+        private class Rect : Shape
+        {
+            public Rect()
+            {
+                this.indexPublic = 0;
+                this.indexProtected = 0;
+                //this.indexInternal = 0;
+                this.indexProtectedInternal = 0;
+            }
+        }
+
     }
 }
